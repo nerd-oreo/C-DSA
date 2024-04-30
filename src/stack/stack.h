1e22ce9fbf0bb4e64ad4ff2ecf722046da0d9fde
Tan <tantr.us91@gmail.com>
2024-04-29 21:24:08 -0700

    define pointer as Cpp style

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -6,16 +6,16 @@
 struct Stack {
     int top;
     size_t size;
-    void *data; 
+    void* data; 
 };
 
 typedef Stack Stack;
 Stack* Stack_init(size_t stack_size, size_t type_size);
-void Stack_push(void *data);
-void* Stack_pop(Stack *stack);
-int Stack_isFull(Stack *stack);
-int Stack_isEmpty(Stack *stack);
-void* Stack_top(Stack *stack);
+void Stack_push(Stack* stack, void* data);
+void* Stack_pop(Stack* stack);
+int Stack_isFull(Stack* stack);
+int Stack_isEmpty(Stack* stack);
+void* Stack_top(Stack* stack);
 
 #undef STACK_H
 #endif
