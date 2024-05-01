c9d16934b987929533fde112ff0c37cb75fc28ab
Travis_Tran <duytantran2710@gmail.com>
2024-04-30 21:49:40 -0700

    prototyping stack interface

---
+++ src/stack.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_SIZE 100
+
+typedef struct ArrayStack
+{
+    size_t size;
+    int top;
+    void *stack[MAX_SIZE];
+} ArrayStack;
+
+// ArrayStack operations
+ArrayStack *ArrayStack_init(size_t type_size);
+void ArrayStack_push(ArrayStack *stack, void *data, size_t type_size);
+void *ArrayStack_pop(ArrayStack *stack);
+void *ArrayStack_peek(ArrayStack *stack);
+void ArrayStack_clear(ArrayStack *stack);
+bool ArrayStack_isEmpty(ArrayStack *stack);
+bool ArrayStack_isFull(ArrayStack *stack);
+void ArrayStack_debug(ArrayStack *stack); // print stack info
+#undef STACK_H
+#endif
