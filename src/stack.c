39bcf7279d0754868d63b5965d34baabc10fa426
Tan <tantr.us91@gmail.com>
2024-05-01 22:25:28 -0700

    Implement ArrayStack_push function - push data to the stack
    Implement ArrayStack_isEmpty function - check if the stack is empty
    
    Implement ArrayStack_isFull function - check if the stack is full
    
    Modify ArrayStack_debug to print the data in the stack from the top.

--- src/stack.c
+++ src/stack.c
@@ -7,18 +7,67 @@ ArrayStack *ArrayStack_init()
     if (stack != NULL)
     {
         stack->size = 0;
-        stack->top = -1; // top index increase by 1 whenever new data is pushed to the stack
+        stack->top = 0; // top index increase by 1 whenever new data is pushed to the stack
         return stack;
     }
     return NULL; // fail to allocate memory
 }
 
+void ArrayStack_push(ArrayStack *stack, void *data)
+{
+    if (ArrayStack_isEmpty(stack) || !ArrayStack_isFull(stack))
+    {
+        stack->top++;
+        stack->data[stack->top] = data;
+        stack->size++;
+    }
+    else
+    {
+        fprintf(stderr, "Stack Error: The stack is full.\n");
+    }
+}
 
-void ArrayStack_debug(ArrayStack *stack)
+bool ArrayStack_isEmpty(ArrayStack *stack)
+{
+    return stack->size == 0;
+}
+
+bool ArrayStack_isFull(ArrayStack *stack)
+{
+    return stack->top == stack->size-1;
+}
+
+void ArrayStack_debug(ArrayStack *stack, char type)
 {
     if (stack != NULL)
     {
-        printf("stack->size: %lu\n", stack->size);
+        printf("stack->size: %llu\n", stack->size);
         printf("stack->top: %d\n", stack->top);
+        printf("----------------------------------\n\n");
+        // Print the elements from the top
+        for (int index = stack->top; index > 0; index--)
+        {
+            void *data = stack->data[index];
+            printf("stack[%d]: ", index);
+            switch (type)
+            {
+                case 'i':   // integer
+                    printf("%d\n", *(int*)data);
+                    break;
+                case 'c':
+                    printf("%c\n", *(char*)data);
+                    break;
+                case 'f':
+                    printf("%f\n", *(float*)data);
+                    break;
+                default:
+                    printf("unspecified type\n");
+                    break;
+            }
+        }
+    }
+    else 
+    {
+        printf("The stack is null.\n");
     }
 }
