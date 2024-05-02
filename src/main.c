b793cae92f025cc017b34d5d2088f673cf9c86ce
Tan <tantr.us91@gmail.com>
2024-05-01 22:21:16 -0700

    Implement the test code for ArrayStack_push function
    Base on the number of test data (nItem), the loop will allocation
    memory to hold the data and store a random integer. The data will
    be pushed to the stack.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "common.h"
 #include "stack.h"
@@ -21,7 +22,12 @@ void stack_test()
     ArrayStack *stack = ArrayStack_init();
 
     // Add data to the stack
+    for (int i = 0; i < nItem; i++)
+    {
+        int *data = (int*) malloc(sizeof(int));
+        *data = random_number(1, 100);
+        ArrayStack_push(stack, data);
+    }
 
-
-    ArrayStack_debug(stack);
+    ArrayStack_debug(stack, 'i');
 }
