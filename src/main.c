b6b0a3dfd055b631625c8a0ecc80d0ecc2e91ebf
Tan <tantr.us91@gmail.com>
2024-05-01 21:24:50 -0700

    Create stack_test function for testing the Stack, seperate the test code for other future test

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "common.h"
 #include "stack.h"
+
+void stack_test();
 
 int main()
 {
-    ArrayStack *stack = ArrayStack_init();
-
-    ArrayStack_debug(stack);
+    stack_test();
 
     return 0;
+}
+
+void stack_test()
+{
+    srand(time(NULL));
+    int nItem = 5;
+
+    ArrayStack *stack = ArrayStack_init();
+
+    // Add data to the stack
+
+
+    ArrayStack_debug(stack);
 }
