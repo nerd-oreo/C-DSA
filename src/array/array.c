428cc7153a6e2f8df69d3c07d0be797d43fe7b2e
Tan <tantr.us91@gmail.com>
2024-05-08 21:28:21 -0700

    add file description

--- src/array/array.c
+++ src/array/array.c
@@ -1,3 +1,7 @@
+/*  @file       array.c
+ *  @brief      This file contains the implementations of dynamic array operations.
+ *  @author     Duy Tran
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "array.h"
