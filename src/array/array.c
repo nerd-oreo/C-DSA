4bc3a4ff6d98e2745c6eca0f79fee3e09c9b9d7d
Duy Tran <duytantran2710@gmail.com>
2024-05-08 21:48:50 -0700

    Implement Array_init

--- src/array/array.c
+++ src/array/array.c
@@ -5,3 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "array.h"
+
+/*  @brief      Initialize new array
+*   @param      capacity The number of elements the array can hold.
+*   @param      typeSize Size of the data type that being stored in the array.
+*   @return     Return the address to the new array
+*/
+Array * Array_init(size_t capacity, size_t sizeType)
+{
+    Array *array = (Array*) malloc(sizeof(Array));
+    if (array != NULL)
+    {
+        array->elements = malloc(capacity * sizeType);
+        array->capactity = capacity;
+        array->size = 0;
+        return array;
+    }
+    fprintf(stderr, "Failed to allocate memory. \n");
+    return NULL;
+}
