99192fad3b8d0ce475bd4adf40b334cfa04abc69
Duy Tran <duytantran2710@gmail.com>
2024-05-10 22:02:24 -0700

    test Array_append

--- test/array/main.c
+++ test/array/main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <common/common.h>
 #include <array/array.h>
 
 int main()
 {
+    srand(time(NULL));
     size_t capacity = 20;
     int test_time = 16;
 
     // Initialize array
-    Array *array = Array_init(capacity, sizeof(int));
+    Array *array = Array_init(capacity, sizeof(int*));
 
     // Test Array_debug()
+    Array_debug(array, 'i');
+
+    // Add data to array
+    for (int i = 0; i < test_time; i++)
+    {
+        int *data = (int*) malloc(sizeof(int));
+        *data = random_number(1, 500);
+        Array_append(array, data);
+    }
+
+    // Print array data
     Array_debug(array, 'i');
 }
