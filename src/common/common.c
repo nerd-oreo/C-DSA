ab5ff129b45d83a9771e189f8d56d1a8a86370ef
Travis_Tran <duytantran2710@gmail.com>
2024-05-11 21:02:48 -0700

    Add function documentation

--- src/common/common.c
+++ src/common/common.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "common.h"
 
+/**
+ * @brief   Generate a random integer between a specified range.
+ *
+ * @param min   A minimum range number
+ * @param max   A maximum range number
+ * @return ** int   Return a random integer
+ */
+
 int random_number(int min, int max)
 {
     return rand() % (((max + 1) - min) + min);
