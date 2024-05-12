6300ee3d4d8dd192b5c929b53e709f82a199c5d7
Travis_Tran <duytantran2710@gmail.com>
2024-05-11 21:09:16 -0700

    update function usage for random_integer()

--- src/common/common.c
+++ src/common/common.c
@@ -6,9 +6,8 @@
  *
  * @param min   A minimum range number
  * @param max   A maximum range number
- * @return ** int   Return a random integer
+ * @return int   Return a random integer
  */
-
 int random_number(int min, int max)
 {
     return rand() % (((max + 1) - min) + min);
