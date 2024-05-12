b80d00e9c312c951bd85cea540dedb656f00b604
Travis_Tran <duytantran2710@gmail.com>
2024-05-11 21:13:07 -0700

    Implement size, capacity, isEmpty functions and document function usage

--- src/array/array.c
+++ src/array/array.c
@@ -1,22 +1,25 @@
-/*  @file       array.c
+/**
+ *  @file       array.c
  *  @brief      This file contains the implementations of dynamic array operations.
  *  @author     Duy Tran
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "array.h"
 
-/*  @brief      Initialize new array
-*   @param      capacity The number of elements the array can hold.
-*   @param      typeSize Size of the data type that being stored in the array.
-*   @return     Return the address to the new array
-*/
-Array * Array_init(size_t capacity, size_t sizeType)
+/**
+ *  @brief      Initialize new array
+ *  @param      capacity    The number of elements the array can hold.
+ *  @param      typeSize    Size of the data type that being stored in the array.
+ *  @return     Return the address to the new array
+ */
+Array *Array_init(size_t capacity, size_t sizeType)
 {
-    Array *array = (Array*) malloc(sizeof(Array));
+    Array *array = (Array *)malloc(sizeof(Array));
     if (array != NULL)
     {
-        array->elements = (void**)malloc(capacity * sizeType);
+        array->elements = (void **)malloc(capacity * sizeType);
         array->capactity = capacity;
         array->size = 0;
         return array;
@@ -25,10 +28,12 @@ Array * Array_init(size_t capacity, size_t sizeType)
     return NULL;
 }
 
-/*  @brief      Append an element to the end of the array.
-*   @param      *array: The address to the allocated array.
-*   @param      *data:  The address to the data.
-*/
+/** ADDING OPERATIONS **/
+/**
+ *  @brief      Append an element to the end of the array.
+ *  @param      *array      The address to the allocated array.
+ *  @param      *data       The address to the data.
+ */
 void Array_append(Array *array, void *data)
 {
     if (array->size < array->capactity)
@@ -38,10 +43,55 @@ void Array_append(Array *array, void *data)
     }
 }
 
-/*  @brief      Print array data for debugging purpose.
-*   @param      array:      The address to the array.
-*   @param      dataType:   Indicate data type for printing format.  
-*/
+/** REMOVING OPERATIONS **/
+
+/** ACCESSING OPERATIONS **/
+
+/** SHIFTING OPERATIONS **/
+
+/** SIZE AND CAPACITY OPERATIONS **/
+/**
+ *  @brief      Get the size of the array
+ *  @param      *array      The address of the array
+ *  @return     Return the size of the array
+ */
+size_t Array_size(Array *array)
+{
+    return array->size;
+}
+
+/**
+ * @brief           Get the capacity of the array
+ *
+ * @param array     The address of the array
+ * @return size_t   Return the capacity of the array
+ */
+size_t Array_capacity(Array *array)
+{
+    return array->capactity;
+}
+
+/**
+ * @brief           Check if the array is empty
+ *
+ * @param array     The address of the array
+ * @return     Return true if the array is empty, otherwise false.
+ */
+bool Array_isEmpty(Array *array)
+{
+    return array->size == 0;
+}
+
+/** SEARCH OPERATIONS **/
+
+/** SORT OPERATIONS **/
+
+/** OTHER OPERATIONS **/
+/**
+ *  @brief      Print array data for debugging purpose.
+ *  @param      array:      The address to the array.
+ *  @param      dataType:   Indicate data type for printing format.
+ */
 void Array_debug(Array *array, char dataType)
 {
     printf("Capacity: %lld\n", array->capactity);
@@ -54,20 +104,20 @@ void Array_debug(Array *array, char dataType)
         for (int i = 0; i < array->size; i++)
         {
             printf("array[%2d]: ", i);
-            switch(dataType)
+            switch (dataType)
             {
-                case 'i':
-                    printf("%d\n", *((int*)array->elements[i]));
-                    break;
-                case 'c':
-                    printf("%c\n", *((char*)array->elements[i]));
-                    break;
-                case 'f':
-                    printf("%f\n", *((float*)array->elements[i]));
-                    break;
-                default:
-                    printf("unspecified\n");
-                    break;
+            case 'i':
+                printf("%d\n", *((int *)array->elements[i]));
+                break;
+            case 'c':
+                printf("%c\n", *((char *)array->elements[i]));
+                break;
+            case 'f':
+                printf("%f\n", *((float *)array->elements[i]));
+                break;
+            default:
+                printf("unspecified\n");
+                break;
             }
         }
     }
