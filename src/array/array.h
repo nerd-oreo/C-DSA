7ba7cd0d473e010f8b7056ad56804192db5ed298
Travis_Tran <duytantran2710@gmail.com>
2024-05-11 21:12:27 -0700

    Remove operations comment

--- src/array/array.h
+++ src/array/array.h
@@ -1,4 +1,5 @@
-/*  @file       array.h
+/**  
+ *  @file       array.h
  *  @brief      This file declare the interface for dynamic array data structure.
  *  @author     Duy Tran
  */
@@ -16,38 +17,30 @@ typedef struct Array
 
 Array *Array_init(size_t capactity, size_t sizeType);
 
-// Adding operations
 void Array_append(Array *array, void *data);              // Add last
 void Array_prepend(Array *array, void *data);             // Add first
 void Array_insertAt(Array *array, int index, void *data); // Insert data at a specifc index
 
-// Removing operations
 void Array_removeFirst(Array *array);
 void Array_removeLast(Array *array);
 void Array_removeAt(Array *array, int index);
 
-// Accessing operations
 void *Array_get(Array *array, int index);
 void *Array_getFirst(Array *array);
 void *Array_getLast(Array *array);
 
-// Shifting
 void Array_shiftLeft(Array *array);
 void Array_shiftRight(Array *array);
 
-// Size and Capacity operations
 size_t Array_size(Array *array);
 size_t Array_capacity(Array *array);
 void Array_resize(Array *array, size_t newSize);
 bool Array_isEmpty(Array *array);
 
-// Search operations
 int Array_search(Array *array, void *value); // Return value index
 
-// Sort Operations
 void Array_sort(Array *array);
 
-// Other operations
 void Array_clear(Array *array);
 void Array_debug(Array *array, char dataType);
 #endif // ARRAY_H
